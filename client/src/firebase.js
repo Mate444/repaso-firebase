4b03ab9dd7c254f40e1850a764e031d28fc66738
mate444 <mateoeo23@gmail.com>
2021-11-17 16:31:43 -0300

    added firebase

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBwKkDVMS7ka83_a45xp2W5WIpQk6IwJGc",
+  authDomain: "repaso-coder.firebaseapp.com",
+  projectId: "repaso-coder",
+  storageBucket: "repaso-coder.appspot.com",
+  messagingSenderId: "815036485514",
+  appId: "1:815036485514:web:06a227f34ad4e1c70b4273"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
